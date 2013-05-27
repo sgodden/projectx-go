b32603835751669d6959b8ad69c510338f4aada0
Simon Godden <simon@blackcatsolutions.co.uk>
2013-05-27 18:53:51 +0100

    Slight refactor of test

--- persistence/IdentifiableConverter_test.go
+++ persistence/IdentifiableConverter_test.go
@@ -15,9 +15,7 @@ func TestIdentifiableConverterShouldAllocateId(t *testing.T) {
 	value, present := m["_id"]
 	if !present {
 		t.Error("_id is not present")
-	}
-
-	if reflect.TypeOf(value) != reflect.TypeOf(bson.NewObjectId()) {
+	} else if reflect.TypeOf(value) != reflect.TypeOf(bson.NewObjectId()) {
 		t.Error(fmt.Sprintf("_id is wrong type: %T", value))
 	}
 
---
+++ testutil/TestUtil.go
@@ -0,0 +1,12 @@
+package testutil
+
+import "testing"
+
+
+func AssertLength(s interface {}, msg string, t *testing.T) {
+	if len(s.(string)) == 0 {
+		t.Error(msg)
+	}
+}
+
+
