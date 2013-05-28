5644d943aaa6cfb124aa361b3be8d77e9f5490b9
Simon Godden <simon@blackcatsolutions.co.uk>
2013-05-28 12:57:19 +0100

    Validation renaming

--- validation/Validation_test.go
+++ validation/Validation_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-func TestEmptyValidatorDoesNotAllowEmptyString(t *testing.T) {
+func TestNotEmptyConstraintDoesNotAllowEmptyString(t *testing.T) {
 	success, _ := NotEmptyConstraint{}.Validate("", "foo")
 	if success {
 		t.Error("Should have failed")
 	}
 }
-func TestEmptyValidatorAllowsNonEmptyString(t *testing.T) {
+func TestNotEmptyConstraintAllowsNonEmptyString(t *testing.T) {
 	success, _ := NotEmptyConstraint{}.Validate("bar", "foo")
 	if !success {
 		t.Error("Should have passed")
