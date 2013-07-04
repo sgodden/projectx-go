2575665f158edc5aba71feebf890849abfb79bcb
Simon Godden <simon@blackcatsolutions.co.uk>
2013-07-04 21:59:33 +0100

    Add some docs

--- persistence/base/BaseRepository.go
+++ persistence/base/BaseRepository.go
@@ -17,12 +17,18 @@ func _createSession() {
 	session = s
 }
 
+// Initialises the mongo session
 func InitSession() {
 	_createSession()
 }
 
+// Converter converts a map to an object and vice versa
 type Converter interface {
+
+	// Takes a map and returns an object
 	FromMap(m map[string]interface {}, o interface {})
+
+	// Takes an object and returns a BSON map
 	ToBson(o interface {}) bson.M
 }
 
@@ -32,22 +38,23 @@ type Repository interface {
 	Converter() Converter
 }
 
-func GetColl(collectionName string) *mgo.Collection {
+func getColl(collectionName string) *mgo.Collection {
 	if session == nil {
 		panic("No mongo session - ensure you have called InitSession on startup")
 	}
 	return session.DB("go-projectx").C(collectionName)
 }
 
+// Finds an object by its identity
 func FindById(id string, r Repository) interface {} {
 	m := make(map[string]interface{})
 	o := r.NewObject()
-	GetColl(r.CollectionName()).FindId(bson.ObjectIdHex(id)).One(&m)
+	getColl(r.CollectionName()).FindId(bson.ObjectIdHex(id)).One(&m)
 	r.Converter().FromMap(m, o)
 	return o
 }
 
 func Save(o interface {}, r Repository) {
-	GetColl(r.CollectionName()).Insert(r.Converter().ToBson(o))
+	getColl(r.CollectionName()).Insert(r.Converter().ToBson(o))
 }
 
