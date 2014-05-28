755fbe4d0e3066e43cbfbfb2b382cf652eaf384e
Simon Godden <simon@blackcatsolutions.co.uk>
2014-05-28 17:32:00 +0100

    Basic stuff to access JIRA REST API

--- src/projectx/main.go
+++ src/projectx/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"projectx/persistence"
+	"net/http"
 )
 
 func main() {
+	client := &http.Client {}
+	req, _ := http.NewRequest("GET", "https://blackcatsolutions.atlassian.net/rest/api/2/project", nil)
+	req.Header.Add("Authorization", "Basic YWRtaW5pc3RyYXRvcjo0bXkgaGFtbSNyaGVhZA==")
+	resp, _ := client.Do(req)
+	fmt.Println(resp.Status)
+	
+	return
+	
 	repo := persistence.NewCustomerOrderRepository()
 
 	fmt.Println(repo.FindById(1))
