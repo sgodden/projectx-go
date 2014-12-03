eb26df5da3cf0c5fb581d11ca9bc56b2db2f10cc
Simon Godden <simon@blackcatsolutions.co.uk>
2014-12-03 16:25:00 +0000

    Fix up the handlers so that we can directly access bookmarked URLs

--- src/projectx/main.go
+++ src/projectx/main.go
@@ -8,43 +8,27 @@ import (
 )
 
 func main() {
-	
-	// order := model.CustomerOrder{}
-	// order.OrderNumber = "ASDASD"
-	// order.CustomerReference = "WERWER"
-	// order.Status = "CREATED"
-	//
-	// repo.Save(&order)
-	//
-	// fmt.Println(order.Id)
-	//
-	// reloadedOrder := repo.Get(order.Id)
-	// fmt.Println(reloadedOrder.Id)
-	// fmt.Println(reloadedOrder.Status)
-	//
-	// orders := repo.Query()
-	//
-	// for _, order := range orders {
-	// 	fmt.Println(order.Id)
-	// }
-	
-	/* let's start a web server */
-	
+		
 	r := mux.NewRouter()
 	
-	fs := http.FileServer(http.Dir("static"))
 	redirectToIndex := http.RedirectHandler("/index.htm", 302)
 	
 	r.Handle("", redirectToIndex)
 	r.Handle("/", redirectToIndex)
-	r.PathPrefix("/index.htm/").Handler(redirectToIndex)
 	
-	r.Handle("/index.htm", fs)
-	r.PathPrefix("/app/").Handler(fs)
+	r.PathPrefix("/index.htm").HandlerFunc(serveIndex)
+	
+	fileServer := http.FileServer(http.Dir("static"))
+	r.PathPrefix("/app/").Handler(fileServer)
 	
 	s := r.PathPrefix("/rest").Subrouter()
 	resources.Configure(s)
 	
 	fmt.Println("Listening on port 8080...")
+
 	http.ListenAndServe(":8080", r)
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.htm")
 }
