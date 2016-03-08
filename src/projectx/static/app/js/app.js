1a590b5fd9abd5330069d4b70b7c61465729cd53
Simon Godden <simon@blackcatsolutions.co.uk>
2016-03-08 19:01:01 +0000

    Remove errant comma and reformat the controller

--- src/projectx/static/app/js/app.js
+++ src/projectx/static/app/js/app.js
@@ -1,25 +1,24 @@
 "use strict";
 
 angular.module('projectx', [
-	'ngResource',
-	'ngRoute',
-])
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	$routeProvider.when('/orders', { templateUrl: '/app/js/partials/orders.html', controller: 'ordersController' });
-	$routeProvider.when('/order/:id', { templateUrl: '/app/js/partials/order.html', controller: 'orderController' });
-	$routeProvider.otherwise({ redirectTo: '/orders' });
-	
-	$locationProvider.html5Mode({
-		enabled: true
-	});
-}])
-.factory('Order', ['$resource', function($resource) {
-	return $resource('/rest/order/:id');
-}])
-.controller('ordersController', ['$scope', 'Order', function($scope, Order) {
-	$scope.orders = Order.query();
-}])
-.controller('orderController', ['$scope', 'Order', '$routeParams', function($scope, Order, routeParams) {
-	$scope.order = Order.get({ id: routeParams.id });
-}])
-;
+		'ngResource',
+		'ngRoute'
+	])
+	.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+		$routeProvider.when('/orders', {templateUrl: '/app/js/partials/orders.html', controller: 'ordersController'});
+		$routeProvider.when('/order/:id', {templateUrl: '/app/js/partials/order.html', controller: 'orderController'});
+		$routeProvider.otherwise({redirectTo: '/orders'});
+
+		$locationProvider.html5Mode({
+			enabled: true
+		});
+	}])
+	.factory('Order', ['$resource', function ($resource) {
+		return $resource('/rest/order/:id');
+	}])
+	.controller('ordersController', ['$scope', 'Order', function ($scope, Order) {
+		$scope.orders = Order.query();
+	}])
+	.controller('orderController', ['$scope', 'Order', '$routeParams', function ($scope, Order, routeParams) {
+		$scope.order = Order.get({id: routeParams.id});
+	}]);
