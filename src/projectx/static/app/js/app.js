d2beac16eb0b3589e5f2aad0322facf30cb4586c
Simon Godden <simon@blackcatsolutions.co.uk>
2016-03-08 19:10:41 +0000

    Ability to save a new order

--- src/projectx/static/app/js/app.js
+++ src/projectx/static/app/js/app.js
@@ -4,21 +4,37 @@ angular.module('projectx', [
 		'ngResource',
 		'ngRoute'
 	])
+
 	.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 		$routeProvider.when('/orders', {templateUrl: '/app/js/partials/orders.html', controller: 'ordersController'});
 		$routeProvider.when('/order/:id', {templateUrl: '/app/js/partials/order.html', controller: 'orderController'});
+		$routeProvider.when('/neworder', {templateUrl: '/app/js/partials/order.html', controller: 'orderController'});
 		$routeProvider.otherwise({redirectTo: '/orders'});
 
 		$locationProvider.html5Mode({
 			enabled: true
 		});
 	}])
+
 	.factory('Order', ['$resource', function ($resource) {
 		return $resource('/rest/order/:id');
 	}])
-	.controller('ordersController', ['$scope', 'Order', function ($scope, Order) {
+
+	.controller('ordersController', ['$scope', 'Order', '$location', function ($scope, Order, $location) {
 		$scope.orders = Order.query();
+		$scope.new = function () {
+			$location.path('/neworder');
+		};
 	}])
+
 	.controller('orderController', ['$scope', 'Order', '$routeParams', function ($scope, Order, routeParams) {
-		$scope.order = Order.get({id: routeParams.id});
+		if (routeParams.id) {
+			$scope.order = Order.get({id: routeParams.id});
+		} else {
+			$scope.order = { OrderNumber: undefined};
+		}
+
+		$scope.submit = function () {
+			Order.save($scope.order);
+		}
 	}]);
